cf27e0093e347bc92b8afeaf47d7dc20b2964d53
张集伟 <zhangjw01@mingyuanyun.com>
2019-10-31 18:22:20 +0800

    testing

--- scripts/test.js
+++ scripts/test.js
@@ -1,9 +1,27 @@
+const through = require('through2')
+const babel = require('@babel/core')
+
+const babelTransform = (code) => {
+    return babel.transformSync(code, {
+        sourceMaps: true,
+        configFile: path.resolve(__dirname, './config/bable.config.js'), 
+    })
+}
+
+compileTs(through(function(vinyl, enc, callback) {
+
+    this.push(vinyl);
+    callback()
+}))
+
 const fs = require('fs')
+const path = require('path')
 
 require("@babel/core").transformFile('../src/components/Button/index.tsx', {
-    presets: ["@babel/preset-typescript", {allExtensions: true, isTSX: true}]
+    sourceMaps: true,
+    configFile: path.resolve(__dirname, './config/bable.config.js'),
 }, (err, res) => {
     if (err) throw  err;
     fs.writeFileSync('./temp/code.js', res.code)
-    fs.writeFileSync('./temp/code.js.map', res.map)
+    fs.writeFileSync('./temp/code.js.map', JSON.stringify(res.map))
 })
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 
+enum t {
+    a,
+    b,
+}
 interface PropType {
-    onClick: (e) => void
+    onClick: (e: any) => void
 }
 class Button extends React.Component<PropType, any> {
     render() {
+        console.log(t.a)
         return <button className="wa-button" onClick={this.props.onClick}>
             {this.props.children}
         </button>
